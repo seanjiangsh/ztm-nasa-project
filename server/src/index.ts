de0adac8d3a49fcf8ff605a89b97c559cc67761e
sean <sean.jiang@hyperdigital.com.tw>
2023-03-18 20:37:31 +0800

    Added MongoDB Atlas connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,23 @@
 import http from "http";
+import fs from "fs";
+
+import mongoose from "mongoose";
 
 import app from "./app";
 import { loadPlanetsData } from "./models/planets.model";
 
 const PORT = process.env.PORT || 5000;
+const MONGO_KEY = fs.readFileSync("mongo.key", "utf-8");
+
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () =>
+  console.log("MongoDB connection ready.")
+);
+mongoose.connection.once("error", (err) => console.log(err));
+
 async function startServer() {
+  await mongoose.connect(MONGO_KEY);
   await loadPlanetsData();
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
