69d611e2105b457c676b6ccbc2817fca499575b2
sean <seanjiangsh@gmail.com>
2023-03-26 21:38:16 +0800

    Changed MongoURL to use dotent, fixed pagination bug

--- server/src/services/mongo.ts
+++ server/src/services/mongo.ts
@@ -1,7 +1,11 @@
 import fs from "fs";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const MONGO_KEY = fs.readFileSync("mongo.key", "utf-8");
+dotenv.config();
+
+// const MONGO_URL = fs.readFileSync("mongo.key", "utf-8");
+const { MONGO_URL } = process.env;
 
 mongoose.connection.once("open", () =>
   console.log("MongoDB connection ready.")
@@ -12,7 +16,8 @@ mongoose.connection.once("close", () =>
 mongoose.connection.once("error", (err) => console.log(err));
 
 export async function mongoConnect() {
-  await mongoose.connect(MONGO_KEY);
+  if (!MONGO_URL) throw new Error("MongoDB connection URL not exists in .env");
+  await mongoose.connect(MONGO_URL);
 }
 
 export async function mongoDisconnect() {
--- server/src/services/query.ts
+++ server/src/services/query.ts
@@ -5,8 +5,8 @@ const DEFAULT_PAGE_LIMIT = 0; // * get all (no limit)
 
 function getPagination(req: Request) {
   const { limit, page } = req.query;
-  const l = Math.abs(Number(limit)) || DEFAULT_PAGE_NUMBER;
-  const p = Math.abs(Number(page)) || DEFAULT_PAGE_LIMIT;
+  const l = Math.abs(Number(limit)) || DEFAULT_PAGE_LIMIT;
+  const p = Math.abs(Number(page)) || DEFAULT_PAGE_NUMBER;
   const skip = (p - 1) * l;
   return { limit: l, skip };
 }
